a739df679ec1edc291130d834ae3a5f8e297b8e6
Саттаров Айрат Камилевич (vtb4054235) <aksattarov@vtb.ru>
2023-06-30 17:18:35 +0300

    changed: function range_id, was added dynamic return question

--- auxiliary_package/range_function.py
+++ auxiliary_package/range_function.py
@@ -1,10 +1,17 @@
+from random import randrange
 
 class Range():
     def range_id(self, diapason: int):
         l_st = []
-        for i in range(1, diapason, 2):
-            l_st.append(i)
+        while len(l_st) < 3:
+            for i in range(1, diapason):
+                l = randrange(1, diapason, 2)
+                if l not in l_st:
+                    l_st.append(l)
+                if len(l_st) > 3:
+                    break
         return l_st
 
 if __name__ == "__main__":
-    print(Range.range_id(6))
+    print(Range().range_id(12))
+
